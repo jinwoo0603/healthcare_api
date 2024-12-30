87cc0f3dbe85aac00a3c1f374984f592f5f2a6ad
jinwoo baek <jinwoo060360@gmail.com>
2024-12-30 19:02:16 +0900

    yeah not this part tho my bad

--- models.py
+++ models.py
@@ -8,8 +8,13 @@ db = SQLAlchemy()
 # Role-User 관계 테이블
 roles_users = db.Table(
     'roles_users',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
 )
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
